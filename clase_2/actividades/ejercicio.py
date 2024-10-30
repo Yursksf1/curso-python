9e3d2f80de744b90539d1ffdd88c1e51c8f49fe2
YursKsf1 <yurs.ksf@gmail.com>
2024-10-29 21:17:23 -0500

    agregar ejercicios

---
+++ clase_2/actividades/ejercicio.py
@@ -0,0 +1,39 @@
+"""
+1) crea un programa en python que imprimir los siguientes patrones
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+2) Crea un programa en python que permita al usario deteminar si un correo electronico es valido
+para esto se deben cumplir los siguientes criterios:
+- debe tener el caracter @
+- dente tener el caracter . despues del @ 
+- debe tener un dominio valido ejemplo [gmail, hotmail, outlook, yahoo]
+"""
+
+
+"""
+crea un programa que permita validar si un usuario se encuentra inscrito en una lista valida
+el programa al inicia, debe solicitarle al usuario ingresar los nombre de la lista, 
+una vez los ingrese, el programa le preguntara al usuario un nombre para validar 
+
+ejemplo:
+1) Ingresa la lista de personas separada por comas:
+juan,maria,pedro,camila,jhon
+
+2) ingresa el nombre de la persona a validar:
+- si ingresamos juan, entonces el programa dice, que es True
+- si ingresamos marcos, entonces el programa dice que False
+- si ingresamo Maria, entonces el programa dice, que es True   
+- si ingresamo PEDRO, entonces el programa dice, que es True   
+- si ingresamo john, entonces el programa dice, que es False   
+
+
+
+"""
