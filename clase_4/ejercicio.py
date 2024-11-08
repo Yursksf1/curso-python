1029371b8ec10ef10dba2acd98d0dcaad810c1b7
YursKsf1 <yurs.ksf@gmail.com>
2024-11-07 19:54:25 -0500

    planteamiento ejercicio listas y tuplas

---
+++ clase_4/ejercicio.py
@@ -0,0 +1,39 @@
+"""
+Ejercicios:
+1) crea un lista con el nombre de 3 amigos, 
+    - ordenala alfabeticamente
+    - agrega un nombre adicional
+    - imprime el nombre que esta en la ubicacion 2
+    - odena de forma descendente
+
+2) define una tupla con los dias de la semana en minuscula empezando por lunes 
+    - iprimie el tercer dia en mayuscula
+    - iprimie el ultimo dia con la primera letra en mayuscula
+
+2) define una estructura para almacenar el himno nacional, que este por estrofas
+    ejemplo: 
+    {
+        "estrofa 1": '''Cesó la horrible noche 
+                        La libertad sublime 
+                        Derrama las auroras 
+                        De su invencible luz. 
+                        La humanidad entera, 
+                        Que entre cadenas gime, 
+                        Comprende las palabras 
+                        Del que murió en la cruz ''',
+        "estrofa 2": '''"Independencia" grita 
+                        El mundo americano: 
+                        Se baña en sangre de héroes 
+                        La tierra de Colón. 
+                        Pero este gran principio: "el rey no es soberano" 
+                        Resuena, Y los que sufren 
+                        Bendicen su pasión.  ''',
+
+    }
+    - imprimir la estrofa numero 10, 
+    - imprimir el numero de estrofas, 
+
+
+
+
+"""
