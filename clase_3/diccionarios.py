eec1a0f48bd856bb457ee8f279120aefaa4777af
YursKsf1 <yurs.ksf@gmail.com>
2024-11-07 19:00:09 -0500

    solucion diccionarios saludos

--- clase_3/diccionarios.py
+++ clase_3/diccionarios.py
@@ -27,3 +27,49 @@ print(my_diccionario.items())
  Hola mi nombre es Juan Diaz, tengo 28 de edad, vivo en la ciudad de New York
  tengo 2 mascotas, sus nombre son Mimi y Firulais
 '''
+personas = [
+    {
+        "nombre": "diana",
+        'edad': 21,
+        "amigos": [
+            {
+                "nombre": "sofia",
+                'edad': 40
+            },  
+        ]
+    },  
+    {
+        "nombre": "sofia",
+        'edad': 40
+    },  
+    {
+        "nombre": "marcos",
+        'edad': 50
+    },
+]
+
+persona = {
+    "nombre": "Jhonathan",
+    "apellido": "Ramirez",
+    "edad": 31,
+    "ciudad": "New York",
+    "mascotas": ["Mimi", "Firulais"]
+}
+
+mensaje = """
+Hola mi nombre es {0} {1}
+tengo {2} de edad, 
+vivo en la ciudad de {3}
+tengo {4} mascotas, 
+sus nombre son {5} y {6}
+""".format(
+    persona.get('nombre'),
+    persona.get('apellido'),
+    persona.get('edad'),
+    persona.get('ciudad'),
+    len(persona.get('mascotas')),
+    persona.get('mascotas')[0],
+    persona.get('mascotas')[1],
+)
+
+print(mensaje)
