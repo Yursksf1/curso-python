667919f933fcbf954803d736ae7eb0d70614abec
YursKsf1 <yurs.ksf@gmail.com>
2024-11-16 09:44:40 -0500

    solucion con funciones

--- clase_6/ejemplo_fibionnaci.py
+++ clase_6/ejemplo_fibionnaci.py
@@ -2,22 +2,33 @@
 #ejemplo: 1 1 2 3 5 8 13 21 34 55
 
 
+def get_fibonnaci(numero):
+    anterior = 0
+    nuevo = 1
+    resultado = None
+
+    contador = 1
+    lista_f = []
+    while contador <= numero:
+        resultado = anterior + nuevo
+        print (f"{contador}. {anterior} + {nuevo} = {resultado}" )
+        contador = contador + 1
+        anterior = nuevo
+        nuevo = resultado
+        lista_f.append(nuevo)
+    
+    return lista_f
+
+
+
 numero = int(input("Ingresa el numero: "))
 print(numero)
+lista_f = get_fibonnaci(numero)
+print('lista f', lista_f)
 
-anterior = 0
-nuevo = 1
-resultado = None
 
-contador = 1
-lista_f = []
-while contador < numero:
-    contador = contador + 1
-    resultado = anterior + nuevo
-    print (f"{contador}. {anterior} + {nuevo} = {resultado}" )
 
-    anterior = nuevo
-    nuevo = resultado
-    lista_f.append(nuevo)
-
-print(lista_f)
+numero = int(input("Ingresa el numero: "))
+print(numero)
+lista_f = get_fibonnaci(numero)
+print('lista f', lista_f)
