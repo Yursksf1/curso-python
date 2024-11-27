f20ed702564fc82518f72e201d1907e3f54f958a
YursKsf1 <yurs.ksf@gmail.com>
2024-11-26 20:28:59 -0500

    uso de decoradores para el error

--- clase_8/decoradores.py
+++ clase_8/decoradores.py
@@ -1,5 +1,16 @@
 import time
 
+def medir_tiempo(funcion):
+    def wrapper(*args, ** kwargs):
+        inicio = time.time() # Momento en que se inicia la ejecución de la función
+        resultado = funcion(*args, ** kwargs) # Llamada a la funcion original
+        fin = time.time() # Momento en que se finaliza la ejecución de la función
+        tiempo_transcurrido = fin - inicio
+        print(f"La función {funcion.__name__} tardó {tiempo_transcurrido} segundos en ejecutarse.")
+        return resultado
+    return wrapper
+
+@medir_tiempo
 def operacion_costosa(n):
     # Simulamos una operación costosa con un bucle
     suma = 0
@@ -7,6 +18,7 @@ def operacion_costosa(n):
         suma += i
     return suma
 
+@medir_tiempo
 def operacion_costosa_n2(n):
     # Simulamos una operación costosa con un bucle
     suma = 0
@@ -17,22 +29,12 @@ def operacion_costosa_n2(n):
 
 # Ejemplo de uso
 print("empieza")
-inicio = time.time()
 resultado = operacion_costosa(10000)
-fin = time.time()
-tiempo_transcurrido = fin - inicio
-print('la funcion dardo: ', tiempo_transcurrido)
-
 print("El resultado de la operación costosa es:", resultado)
 print("termina")
 
 
 print("empieza")
-inicio = time.time()
 resultado = operacion_costosa_n2(10000)
-fin = time.time()
-tiempo_transcurrido = fin - inicio
-print('la funcion dardo: ', tiempo_transcurrido)
-
 print("El resultado de la operación costosa es:", resultado)
 print("termina")
